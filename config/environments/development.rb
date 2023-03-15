f2e4792afe02467573882c664eff17992804bfdc
Serg Skol <sps676@gmail.com>
2023-03-15 08:34:40 +0100

    Add letter_opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,6 +39,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
@@ -67,6 +71,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
